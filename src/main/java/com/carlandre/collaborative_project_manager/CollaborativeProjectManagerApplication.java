495c140ea9867ddbdf5e674cbc295d3aa6d45e73
Carl Andre <c3036821@newcastle.ac.uk>
2026-06-30 15:19:40 +0100

    Added foundational files from Spring initialiser.

---
+++ src/main/java/com/carlandre/collaborative_project_manager/CollaborativeProjectManagerApplication.java
@@ -0,0 +1,13 @@
+package com.carlandre.collaborative_project_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeProjectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeProjectManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlandre/collaborative_project_manager/CollaborativeProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlandre.collaborative_project_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

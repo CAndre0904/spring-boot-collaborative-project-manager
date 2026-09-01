56330936e1d1da93d95ddc96cb24cdbfad02cdd3
Carl Andre <c3036821@newcastle.ac.uk>
2026-09-01 12:05:46 +0100

    Adding a jsx file for a tasks page and added links between the tasks page and the home page.

--- frontend/src/js/App.jsx
+++ frontend/src/js/App.jsx
@@ -2,17 +2,26 @@ import React from "react";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 import Home from "./Home";
+import Tasks from "./Tasks";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router";
 
 export default function App() {
   return (
+  <>
   <BrowserRouter>
+    <div className="site-links">
+        <Link to="/home">Home </Link>
+        <Link to="/tasks">Tasks </Link>
+    </div>
     <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/tasks" element={<Tasks />} />
     </Routes>
   </BrowserRouter>
+  </>
   );
 }

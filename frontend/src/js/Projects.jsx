cb541d05992cb73b4f157facc879ecc329cbbc2a
Carl Andre <c3036821@newcastle.ac.uk>
2026-09-03 13:12:30 +0100

    Added React for displaying project data but the backend does not successfully send the data yet.

--- frontend/src/js/Projects.jsx
+++ frontend/src/js/Projects.jsx
@@ -3,12 +3,27 @@ import { useNavigate } from "react-router-dom";
 
 function Projects() {
 
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/project")
+          .then(response => response.json())
+          .then(projects => {
+            setProjects(projects);
+          })
+  }, []);
 
   return (
     <div className="projects">
       <div>
         <h1>Projects</h1>
-
+        {projects.map(project =>
+            <div>
+                <h3>{project.name}</h3>
+                <h3>{project.details}</h3>
+                <h3>{project.dueDate}</h3>
+            </div>
+        )}
       </div>
     </div>
   );

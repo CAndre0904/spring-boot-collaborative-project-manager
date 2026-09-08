f586517dc23a8e4a31d19efd40a75311734a7bb8
Carl Andre <c3036821@newcastle.ac.uk>
2026-09-08 09:18:51 +0100

    Added a service and repository for tasks.

---
+++ backend/src/main/java/com/carlandre/collaborative_project_manager/repository/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.carlandre.collaborative_project_manager.repository;
+import com.carlandre.collaborative_project_manager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+}
